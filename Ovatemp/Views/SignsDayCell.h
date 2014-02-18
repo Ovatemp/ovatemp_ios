6ee19f001d44c4b59134dd9fa7519103dd6af8f2
Chris Cahoon <chris.cahoon@gmail.com>
2014-02-18 11:38:52 -0500

    Implement Secondary Signs cell.

--- Ovatemp/Views/SignsDayCell.h
+++ Ovatemp/Views/SignsDayCell.h
@@ -10,5 +10,11 @@
 #import "DayCell.h"
 
 @interface SignsDayCell : DayCell
+@property (weak, nonatomic) IBOutlet UIImageView *opkImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *ferningImageView;
+@property (weak, nonatomic) IBOutlet UIButton *opkNegativeButton;
+@property (weak, nonatomic) IBOutlet UIButton *opkPositiveButton;
+@property (weak, nonatomic) IBOutlet UIButton *ferningNegativeButton;
+@property (weak, nonatomic) IBOutlet UIButton *ferningPositiveButton;
 
 @end
--- Ovatemp/Views/SignsDayCell.m
+++ Ovatemp/Views/SignsDayCell.m
@@ -10,4 +10,57 @@
 
 @implementation SignsDayCell
 
+- (void)updateControls {
+  if(self.day.opk) {
+    self.opkImageView.hidden = FALSE;
+  } else {
+    self.opkImageView.hidden = TRUE;
+  }
+
+  self.opkImageView.hidden = self.day.opk == nil;
+  self.ferningImageView.hidden = self.day.ferning == nil;
+
+  self.opkNegativeButton.selected = [self.day isProperty:@"opk" ofType:OPK_NEGATIVE];
+  self.opkPositiveButton.selected = [self.day isProperty:@"opk" ofType:OPK_POSITIVE];
+
+  self.ferningNegativeButton.selected = [self.day isProperty:@"ferning" ofType:FERNING_NEGATIVE];
+  self.ferningPositiveButton.selected = [self.day isProperty:@"ferning" ofType:FERNING_POSITIVE];
+
+  for(UIButton *button in @[self.opkNegativeButton, self.opkPositiveButton]) {
+    if(button.selected) {
+      self.opkImageView.image = [button imageForState:UIControlStateNormal];
+    }
+  }
+
+  for(UIButton *button in @[self.ferningNegativeButton, self.ferningPositiveButton]) {
+    if(button.selected) {
+      self.ferningImageView.image = [button imageForState:UIControlStateNormal];
+    }
+  }
+}
+
+- (void)initializeControls {
+  // noop
+}
+
+- (IBAction)negativeOpkTapped:(id)sender {
+  [self.day updateProperty:@"opk" withIndex:OPK_NEGATIVE];
+  [self updateControls];
+}
+
+- (IBAction)positiveOpkTapped:(id)sender {
+  [self.day updateProperty:@"opk" withIndex:OPK_POSITIVE];
+  [self updateControls];
+}
+
+- (IBAction)negativeFerningTapped:(id)sender {
+  [self.day updateProperty:@"ferning" withIndex:FERNING_NEGATIVE];
+  [self updateControls];
+}
+
+- (IBAction)positiveFerningTapped:(id)sender {
+  [self.day updateProperty:@"ferning" withIndex:FERNING_POSITIVE];
+  [self updateControls];
+}
+
 @end
