772a9c1fab2045495aca9205be32ea8de45addb8
Chris Cahoon <chris.cahoon@gmail.com>
2014-03-27 16:56:29 -0400

    Wire up the fertility profile intro view. Update meditation icon.

---
+++ Ovatemp/Controllers/FertilityProfileIntroViewController.h
@@ -0,0 +1,16 @@
+//
+//  FertilityProfileIntroViewController.h
+//  Ovatemp
+//
+//  Created by Chris Cahoon on 3/27/14.
+//  Copyright (c) 2014 Back Forty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FertilityProfileIntroViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIImageView *profileImageView;
+@property (weak, nonatomic) IBOutlet UILabel *profileLabel;
+
+@end
---
+++ Ovatemp/Controllers/FertilityProfileIntroViewController.m
@@ -0,0 +1,28 @@
+//
+//  FertilityProfileIntroViewController.m
+//  Ovatemp
+//
+//  Created by Chris Cahoon on 3/27/14.
+//  Copyright (c) 2014 Back Forty. All rights reserved.
+//
+
+#import "FertilityProfileIntroViewController.h"
+#import "User.h"
+
+@interface FertilityProfileIntroViewController ()
+
+@end
+
+@implementation FertilityProfileIntroViewController
+
+- (void)viewWillAppear:(BOOL)animated {
+  NSString *profileName = [[User current].fertilityProfileName capitalizedString];
+  self.profileLabel.text = profileName;
+  self.profileImageView.image = [UIImage imageNamed:profileName];
+}
+
+- (IBAction)nextTapped:(id)sender {
+  [self.navigationController popViewControllerAnimated:FALSE];
+}
+
+@end
