3a4454854e49bddafc82be6b6ce423e13e187ecc
Flip Sasser <flip@inthebackforty.com>
2014-05-19 16:03:08 -0400

    Use iOS analytics tracker

--- Ovatemp/Constants.h
+++ Ovatemp/Constants.h
@@ -58,7 +58,7 @@ UIColor * ColorA(int red, int green, int blue, CGFloat alpha);
 #define STATUS_BAR_HEIGHT 20
 
 // 3rd party API keys
-static NSString * const kGoogleAnalyticsTrackingID = @"UA-32219902-1";
+static NSString * const kGoogleAnalyticsTrackingID = @"UA-32219902-2";
 static NSString * const kGoogleAdwordsConversionID = @"1009399358";
 static NSString * const kGoogleAdwordsConversionLabel = @"Xn3pCKKe1gcQvuyo4QM";
 static NSString * const kHockeyIdentifier = @"6e19f3154c79a220e01f3820bf0f0f06";
