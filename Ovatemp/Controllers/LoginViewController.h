5e66b3b1b58371c42274f81116475f1d0d667c7d
Josh Lieberman <josh.lieberman92@gmail.com>
2014-10-17 13:25:14 -0400

    initial work on login and signup flow

---
+++ Ovatemp/Controllers/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Ovatemp
+//
+//  Created by Josh L on 10/17/14.
+//  Copyright (c) 2014 Back Forty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
