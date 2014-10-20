ed496d42709c5b2aa421669f6e957c3377d536a5
Josh Lieberman <josh.lieberman92@gmail.com>
2014-10-20 16:40:42 -0400

    work on welcome2vc (start trying screen).  now saving date in nsuserdefaults

---
+++ Ovatemp/Controllers/Welcome3ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Welcome3ViewController.h
+//  Ovatemp
+//
+//  Created by Josh L on 10/20/14.
+//  Copyright (c) 2014 Back Forty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Welcome3ViewController : UIViewController
+
+@end
