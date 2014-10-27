faa901ebe5fe3df23a9261a94918e4c4fadb85e1
Josh Lieberman <josh.lieberman92@gmail.com>
2014-10-27 11:15:56 -0400

    extended cycle length to 36 days

--- Ovatemp/Views/CycleLengthTableViewCell.m
+++ Ovatemp/Views/CycleLengthTableViewCell.m
@@ -15,7 +15,7 @@ NSArray *cycleLengthPickerData;
 - (void)awakeFromNib {
     // Initialization code
     
-    cycleLengthPickerData = @[@"26", @"27", @"28", @"29", @"30"];
+    cycleLengthPickerData = @[@"26", @"27", @"28", @"29", @"30", @"31", @"32", @"33", @"34", @"35", @"36"];
     
     self.cycleLengthPicker.delegate = self;
     
