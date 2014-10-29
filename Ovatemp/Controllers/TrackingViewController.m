dd6762ca4536e9b7632f97f9ca6f269c2573c43d
Josh Lieberman <josh.lieberman92@gmail.com>
2014-10-29 17:34:38 -0400

    arrow rotates when drawer opens

--- Ovatemp/Controllers/TrackingViewController.m
+++ Ovatemp/Controllers/TrackingViewController.m
@@ -13,7 +13,7 @@
 
 @interface TrackingViewController () <UIGestureRecognizerDelegate>
 
-@property UIView *arrowView;
+@property UIImageView *arrowImageView;
 
 @end
 
@@ -26,9 +26,7 @@ BOOL lowerDrawer;
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-//    self.title = @"Tracking";
-//    self.navigationItem.prompt = @"subtitle\nhello";
-//    CGRectMake(<#CGFloat x#>, <#CGFloat y#>, <#CGFloat width#>, <#CGFloat height#>)
+
     // title
     CGRect headerTitleSubtitleFrame = CGRectMake(0, -15, 200, 44);
     UIView *_headerTitleSubtitleView = [[UILabel alloc] initWithFrame:headerTitleSubtitleFrame];
@@ -40,12 +38,8 @@ BOOL lowerDrawer;
     titleView.backgroundColor = [UIColor clearColor];
     titleView.font = [UIFont boldSystemFontOfSize:20];
     titleView.textAlignment = NSTextAlignmentCenter;
-//    titleView.textColor = [UIColor whiteColor];
-//    titleView.shadowColor = [UIColor darkGrayColor];
-//    titleView.shadowOffset = CGSizeMake(0, -1);
-    
-    NSDate *date = [NSDate date]; //I'm using this just to show the this is how you convert a date
-    
+
+    NSDate *date = [NSDate date];
     NSDateFormatter *df = [[NSDateFormatter alloc] init];
     [df setDateStyle:NSDateFormatterMediumStyle];
     [df setTimeStyle:NSDateFormatterNoStyle];
@@ -61,51 +55,14 @@ BOOL lowerDrawer;
     subtitleView.backgroundColor = [UIColor clearColor];
     subtitleView.font = [UIFont boldSystemFontOfSize:13];
     subtitleView.textAlignment = NSTextAlignmentCenter;
-//    subtitleView.textColor = [UIColor whiteColor];
-//    subtitleView.shadowColor = [UIColor darkGrayColor];
-//    subtitleView.shadowOffset = CGSizeMake(0, -1);
     subtitleView.text = @"Cycle Day #X";
     subtitleView.adjustsFontSizeToFitWidth = YES;
     
     // arrow
-//    self.arrowView = [[UIView alloc] initWithFrame:CGRectMake(100, 30, 20, 10)];
-//    self.arrowView.backgroundColor = [UIColor blackColor];
-//    
-//    // gesture recognizer
-//    UITapGestureRecognizer *singleTap =
-//    [[UITapGestureRecognizer alloc] initWithTarget: self
-//                                            action: @selector(toggleDrawer:)];
-//    [singleTap setDelegate:self];
-//    singleTap.numberOfTapsRequired = 1;
-//    singleTap.numberOfTouchesRequired = 1;
-//    
-//    [self.arrowView addGestureRecognizer: singleTap];
-//    
-//    [_headerTitleSubtitleView addSubview:self.arrowView];
-//    UIButton *but=[UIButton buttonWithType:UIButtonTypeRoundedRect];
-////    but.frame= CGRectMake(0, 0, 20, 10);
-//    but.frame = CGRectMake(0, 22, 20, 10);
-//    [but setTitle:@"Ok" forState:UIControlStateNormal];
-//    [but addTarget:self action:@selector(toggleDrawer) forControlEvents:UIControlEventTouchUpInside];
-//    self.arrowView = [[UIView alloc] initWithFrame:CGRectMake(50, 10, 20, 10)];
-//    [self.arrowView addSubview:but];
-//    but.backgroundColor = [UIColor redColor];
+    self.arrowImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"icn_pulldown_arrow"]];
     
-    [_headerTitleSubtitleView addSubview:self.arrowView];
-    
-//    // arrow
-//    UIImage *arrowImage = [[UIImage alloc] initWithContentsOfFile:@"icn_pulldown_arrow"];
-//    self.arrowView = [[UIImageView alloc] initWithImage:arrowImage];
-//    
-//    [self.arrowView setFrame:CGRectInset(_headerTitleSubtitleView.bounds, 100, 50)];
-//    
-//    // gesture recognizer
-//    UITapGestureRecognizer *singleFingerTap =
-//    [[UITapGestureRecognizer alloc] initWithTarget:self
-//                                            action:@selector(toggleDrawer:)];
-//    [self.arrowView addGestureRecognizer:singleFingerTap];
-//    
-//    [_headerTitleSubtitleView addSubview:self.arrowView];
+    self.arrowImageView.frame = CGRectMake(90, 30, 20, 10);
+    [_headerTitleSubtitleView addSubview:self.arrowImageView];
     
     [_headerTitleSubtitleView addSubview:subtitleView];
     
@@ -120,8 +77,6 @@ BOOL lowerDrawer;
     [singleTap setDelegate:self];
     singleTap.numberOfTapsRequired = 1;
     singleTap.numberOfTouchesRequired = 1;
-//    [_headerTitleSubtitleView addGestureRecognizer: singleTap];
-//    [[self.navigationController.navigationBar.subviews objectAtIndex:1] setUserInteractionEnabled:YES];
     [self.navigationController.navigationBar addGestureRecognizer:singleTap];
     
     lowerDrawer = YES;
@@ -134,37 +89,32 @@ BOOL lowerDrawer;
 }
 
 - (void)toggleDrawer:(UIGestureRecognizer *)recognizer {
-//    CGPoint location = [recognizer locationInView:[recognizer.view superview]];
-    
-//    NSLog(@"did tap on arrow view");
-    [UIView animateWithDuration:0.3 delay:0.0 usingSpringWithDamping:0.6f initialSpringVelocity:0.5f options:0 animations:^{
+
+    [UIView animateWithDuration:0.5 delay:0.0 usingSpringWithDamping:0.6f initialSpringVelocity:0.5f options:0 animations:^{
         if (lowerDrawer) {
             self.drawerView.frame = CGRectMake(self.drawerView.frame.origin.x, self.drawerView.frame.origin.y + 70, self.drawerView.frame.size.width, self.drawerView.frame.size.height);
             
             // lower table at same time
             self.tableView.frame = CGRectMake(self.tableView.frame.origin.x, self.tableView.frame.origin.y + 70, self.tableView.frame.size.width, self.tableView.frame.size.height);
+            
+            // flip arrow
+            self.arrowImageView.transform = CGAffineTransformMakeRotation(M_PI);
+            
             lowerDrawer = NO;
             
         } else {
             self.drawerView.frame = CGRectMake(self.drawerView.frame.origin.x, self.drawerView.frame.origin.y - 70, self.drawerView.frame.size.width, self.drawerView.frame.size.height);
             
             self.tableView.frame = CGRectMake(self.tableView.frame.origin.x, self.tableView.frame.origin.y - 70, self.tableView.frame.size.width, self.tableView.frame.size.height);
+            
+            // flip arrow
+            self.arrowImageView.transform = CGAffineTransformMakeRotation(M_PI * 180);
+            
             lowerDrawer = YES;
         }
     } completion:^(BOOL finished) {
         //
     }];
-//    [UIView animateWithDuration:0.3 delay:0.1  options:UIViewAnimationOptionCurveEaseIn animations:^{
-//        if (lowerDrawer) {
-//            self.drawerView.frame = CGRectMake(self.drawerView.frame.origin.x, self.drawerView.frame.origin.y + 70, self.drawerView.frame.size.width, self.drawerView.frame.size.height);
-//            lowerDrawer = NO;
-//
-//        } else {
-//            self.drawerView.frame = CGRectMake(self.drawerView.frame.origin.x, self.drawerView.frame.origin.y - 70, self.drawerView.frame.size.width, self.drawerView.frame.size.height);
-//            lowerDrawer = YES;
-//        }
-//    } completion:^(BOOL finished) {
-//            }];
 }
 
 - (IBAction)displayChart:(id)sender {
