e2d99d08e33dfe4a34bb2799bb2365f465dad51e
Josh Lieberman <josh.lieberman92@gmail.com>
2014-10-30 15:57:01 -0400

    work on drawer collection view and notes view controller

--- Ovatemp/Controllers/TrackingViewController.h
+++ Ovatemp/Controllers/TrackingViewController.h
@@ -13,5 +13,6 @@
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @property (weak, nonatomic) IBOutlet UIView *drawerView;
+@property (weak, nonatomic) IBOutlet UICollectionView *drawerCollectionView;
 
 @end
---
+++ Ovatemp/Views/TrackingStatusTableViewCell.h
@@ -0,0 +1,21 @@
+//
+//  TrackingStatusTableViewCell.h
+//  Ovatemp
+//
+//  Created by Josh L on 10/30/14.
+//  Copyright (c) 2014 Back Forty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol TrackingCellDelegate <NSObject>
+
+-(void)pushViewController:(UIViewController *)viewController;
+
+@end
+
+@interface TrackingStatusTableViewCell : UITableViewCell
+
+@property(nonatomic,strong)id<TrackingCellDelegate>delegate;
+
+@end
