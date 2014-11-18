84fa9b450818042f2d60257c02eb0e2a2774fa30
Josh Lieberman <josh.lieberman92@gmail.com>
2014-11-17 21:01:45 -0500

    setting up alarm settings screen in account -> settings view

---
+++ Ovatemp/Controllers/SettingsAlarmViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsAlarmViewController.h
+//  Ovatemp
+//
+//  Created by Josh L on 11/17/14.
+//  Copyright (c) 2014 Back Forty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsAlarmViewController : UIViewController
+
+@end
