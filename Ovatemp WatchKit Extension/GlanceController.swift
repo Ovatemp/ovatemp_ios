22da7357a3fb06ff3481f13eb6676f30cf83a7b4
Arun Venkatesan <blured2000@gmail.com>
2015-01-22 18:48:28 -0500

    implemented glance screen

--- Ovatemp WatchKit Extension/GlanceController.swift
+++ Ovatemp WatchKit Extension/GlanceController.swift
@@ -11,11 +11,41 @@ import Foundation
 
 
 class GlanceController: WKInterfaceController {
+    
+    @IBOutlet weak var fertilityStatusGroup: WKInterfaceGroup!
+    @IBOutlet weak var fertilityStatusLabel: WKInterfaceLabel!
+    
+    let connectionManager = ConnectionManager()
 
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
         // Configure interface objects here.
+        // Configure interface objects here.
+        connectionManager.requestFertilityStatus { (status, error) -> () in
+            
+            switch status {
+                
+            case FertilityStatus.peakFertility:
+                self.fertilityStatusLabel.setText("PEAK FERTILITY")
+                self.fertilityStatusGroup.setBackgroundImageNamed("Fertility Status - fertile")
+            case FertilityStatus.fertile:
+                self.fertilityStatusLabel.setText("FERTILE")
+                self.fertilityStatusGroup.setBackgroundImageNamed("Fertility Status - fertile")
+            case FertilityStatus.notFertile:
+                self.fertilityStatusLabel.setText("NOT FERTILE")
+                self.fertilityStatusGroup.setBackgroundImageNamed("Fertility Status - not fertile")
+            case FertilityStatus.period:
+                self.fertilityStatusLabel.setText("PERIOD")
+                self.fertilityStatusGroup.setBackgroundImageNamed("Fertility Status - period")
+            case FertilityStatus.empty:
+                self.fertilityStatusLabel.setText("Please enter your Basal Body Temperature and other daily data for fertility status.")
+                self.fertilityStatusGroup.setBackgroundImageNamed("Fertility Status - no data")
+            default:
+                self.fertilityStatusLabel.setText("Please enter your Basal Body Temperature and other daily data for fertility status.")
+                self.fertilityStatusGroup.setBackgroundImageNamed("Fertility Status - no data")
+            }
+        }
     }
 
     override func willActivate() {
