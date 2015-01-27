7586e5899e9c2fcf54c41ef3776580e7c312bf04
Arun Venkatesan <blured2000@gmail.com>
2015-01-27 14:27:16 -0500

    network call to handle period button press

--- Ovatemp WatchKit Extension/PeriodPageViewController.swift
+++ Ovatemp WatchKit Extension/PeriodPageViewController.swift
@@ -18,6 +18,8 @@ class PeriodPageViewController: WKInterfaceController {
     @IBOutlet weak var periodSelectMediumButton: WKInterfaceButton!
     @IBOutlet weak var periodSelectHeavyButton: WKInterfaceButton!
     
+    let connectionManager = ConnectionManager()
+    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
     }
@@ -28,5 +30,35 @@ class PeriodPageViewController: WKInterfaceController {
             
             super.becomeCurrentPage()
         }
+    }
+    
+    func resetButtonImages() {
+        
+        self.periodSelectNoneButton.setBackgroundImageNamed("btn_period_none_p")
+        self.periodSelectSpottingButton.setBackgroundImageNamed("btn_period_spotting_p")
+        self.periodSelectLightButton.setBackgroundImageNamed("btn_period_light_p")
+        self.periodSelectMediumButton.setBackgroundImageNamed("btn_period_medium_p")
+        self.periodSelectHeavyButton.setBackgroundImageNamed("btn_period_heavy_p")
+    }
+    
+    @IBAction func didSelectPeriodNone() {
+        
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.locale = NSLocale.systemLocale()
+        dateFormatter.dateFormat = "YYYY-MM-dd'T'HH:mm:ssZZZ"
+        
+        let todayDate = dateFormatter.stringFromDate(NSDate())
+        
+        let selectionDictionary = ["date": todayDate,"period": "none"]
+        
+        connectionManager.updateFertilityData (selectionDictionary, { (success, error) -> () in
+            
+            if(success) {
+                
+                self.periodSelectionLabel.setText("None")
+                self.periodSelectNoneButton.setBackgroundImageNamed("btn_period_none_p")
+            }
+            
+        })
     }
 }
