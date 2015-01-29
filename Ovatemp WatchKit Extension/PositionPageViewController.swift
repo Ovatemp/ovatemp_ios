31385d67f879da3935eb9abe7b1a5f6f3c00085c
Arun Venkatesan <blured2000@gmail.com>
2015-01-29 17:13:49 -0500

    cervical position page initialization

--- Ovatemp WatchKit Extension/PositionPageViewController.swift
+++ Ovatemp WatchKit Extension/PositionPageViewController.swift
@@ -20,4 +20,27 @@ class PositionPageViewController: WKInterfaceController {
     @IBOutlet weak var positionSelectionLabel: WKInterfaceLabel!
     @IBOutlet weak var positionSelectLowClosedFirmButton: WKInterfaceButton!
     @IBOutlet weak var positionSelectHighOpenSoftButton: WKInterfaceButton!
+    
+    var positionSelectedState = PositionState.noData
+    
+    let connectionManager = ConnectionManager()
+    
+    var todayDate : String = ""
+    
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.locale = NSLocale.systemLocale()
+        dateFormatter.dateFormat = "YYYY-MM-dd'T'HH:mm:ssZZZ"
+        
+        self.todayDate = dateFormatter.stringFromDate(NSDate())
+        
+    }
+    
+    func resetButtonImages() {
+        
+        self.positionSelectLowClosedFirmButton.setBackgroundImageNamed("btn_position_lowclosedfirm")
+        self.positionSelectHighOpenSoftButton.setBackgroundImageNamed("btn_position_highopensoft")
+    }
 }
